3e154e34250175248db378097933b2973240c776
Sereny <serejke33@gmail.com>
2026-02-01 14:20:31 +0300

    fix game logic, add basic DOM manipulations

--- script.js
+++ script.js
@@ -1,109 +1,105 @@
-// Есть поле(gameboard) 3х3 (состоит из ячеек) (Объект с массивом клеток внутри, типа матрицы)
-// Есть 2 игрока (Х, О) (Два однотипных объекта с параметрами @name, @marker, factory function)
-
-// Основная логика игры:
-// Игроки ходят по очереди, начиная с Х;
-// Каждый игрок присваивает одной из ячеек Х/О;
-// ХОД ИГРОКА: Выбрать одну из ячеек объекта gameboard.[fieldNumber] = player.mark
-// После каждого хода проверяется выполнение:
-// есть ли три одинаковых символа по вертикали, горизонтали, диагонали? (если да - верни победителя, нет - продолжить проверки)
-// есть ли свободные поля? (если есть - вернуться к ходу игрока; если нет - ничья)
-//
-
-/*  
-Функция createGameboard возвращает поле для игры в крестики-нолики
-
-*/
-
-function createGameboard() {
+function gameActions() {
+    // Создать массив-поле для игры
     const gameboard = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-    return gameboard;
-}
+    // Создать игроков
+    const createPlayer = (marker) => {
+        const name = prompt(`Игрок ${marker} Введите ваше имя`, `${marker}`)
+        return { name, marker }
+    }
 
-/*
-Функция createPlayer() принимает в параметры Имя игрока и Игровой символ
-затем возвращает объект с присвоенными игроку данными 
-*/
-
-function createPlayer(marker) {
-    const name = prompt(`Игрок ${marker} Введите ваше имя`, 'Default')
-    return { name, marker }
-}
-
-/*
-Функция playGame() принимает в параметры новое игровое поле 
-и двух игроков playerХ и playerО
-
-Пока (!gameboard содержит три одинаковых поля подряд 
-    ||!gameboard.includes(0)отсутствуют свободные ячейки) {   
-    
-    Выбор текущего игрока
-
-    Ячейке присваивается marker игрока currentPlayer;
-    makeTurn(index) {
-        index = prompt(`${currentPlayer.name} выбери ячейку`)
-        gameboard[index] = currentPlayer.marker;
-
-    } 
-    
-    Проверка условий:
-        если (gameboard содержит три одинаковых поля подряд) {
-            вернуть победителя
-        }
-        иначе если (!gameboard.includes(0)отсутствуют свободные ячейки) {
-            вернуть сообщение о ничьей
-        }
-
-    changeCurrentPlayer();
-}
-    
-*/
-
-function playGame(gameboard, playerX, playerO) {
-    gameboard = createGameboard();
+    // Создать игроков
     playerX = createPlayer('X');
     playerO = createPlayer('O');
+
+    // Текущий игрок в начале всегда Х
     let currentPlayer = playerX;
 
-    function switchPlayer() {
+    const getCurrentPlayer = () => currentPlayer
+    // Смена текущего игрока
+    const switchPlayer = () => {
         (currentPlayer === playerX)
             ? currentPlayer = playerO
             : currentPlayer = playerX
         return currentPlayer
     };
 
-    function makeTurn(index) {
-        index = prompt(`${currentPlayer.name} выбери ячейку`)
-        gameboard[index] = currentPlayer.marker;
-        currentPlayer = switchPlayer();
-        console.log(gameboard)
-    }
-    while (gameboard.includes(0)) {
-        makeTurn()
-        // проверка победителя
-        if (gameboard[0] === 'X' && gameboard[3] === 'X' && gameboard[6] === 'X'
-        || gameboard[1] === 'X' && gameboard[4] === 'X' && gameboard[7] === 'X'
-        || gameboard[2] === 'X' && gameboard[5] === 'X' && gameboard[8] === 'X'
-        || gameboard[0] === 'X' && gameboard[1] === 'X' && gameboard[2] === 'X'
-        || gameboard[3] === 'X' && gameboard[4] === 'X' && gameboard[5] === 'X'
-        || gameboard[6] === 'X' && gameboard[7] === 'X' && gameboard[8] === 'X'
-        || gameboard[0] === 'X' && gameboard[4] === 'X' && gameboard[8] === 'X'
-        || gameboard[6] === 'X' && gameboard[4] === 'X' && gameboard[2] === 'X') {
-                console.log('X has won')
-                return
-        } else if (gameboard[0] === 'O' && gameboard[3] === 'O' && gameboard[6] === 'O'
-        || gameboard[1] === 'O' && gameboard[4] === 'O' && gameboard[7] === 'O'
-        || gameboard[2] === 'O' && gameboard[5] === 'O' && gameboard[8] === 'O'
-        || gameboard[0] === 'O' && gameboard[1] === 'O' && gameboard[2] === 'O'
-        || gameboard[3] === 'O' && gameboard[4] === 'O' && gameboard[5] === 'O'
-        || gameboard[6] === 'O' && gameboard[7] === 'O' && gameboard[8] === 'O'
-        || gameboard[0] === 'O' && gameboard[4] === 'O' && gameboard[8] === 'O'
-        || gameboard[6] === 'O' && gameboard[4] === 'O' && gameboard[2] === 'O'){
-            console.log('O has won')
-            return
-        } else if (!gameboard.includes(0)) {
-            console.log('Ничья')
+    // Ход игрока
+    const makeTurn = (index) => {
+        if (gameboard[index] !== 0) return;
+
+        gameboard[index] = currentPlayer.marker
+        console.log('done')
+
+        if (!getGameStatus()) {
+            switchPlayer();
         }
     }
-    return { gameboard, playerX, playerO, currentPlayer }
+
+    // Проверка состояния игры
+    const getGameStatus = () => {
+        if (!gameboard.includes(0)) {
+
+            return 'Draw'
+        } else if (gameboard[0] === 'X' && gameboard[3] === 'X' && gameboard[6] === 'X'
+            || gameboard[1] === 'X' && gameboard[4] === 'X' && gameboard[7] === 'X'
+            || gameboard[2] === 'X' && gameboard[5] === 'X' && gameboard[8] === 'X'
+            || gameboard[0] === 'X' && gameboard[1] === 'X' && gameboard[2] === 'X'
+            || gameboard[3] === 'X' && gameboard[4] === 'X' && gameboard[5] === 'X'
+            || gameboard[6] === 'X' && gameboard[7] === 'X' && gameboard[8] === 'X'
+            || gameboard[0] === 'X' && gameboard[4] === 'X' && gameboard[8] === 'X'
+            || gameboard[6] === 'X' && gameboard[4] === 'X' && gameboard[2] === 'X') {
+            console.log('X has won')
+            return 'X'
+        } else if (gameboard[0] === 'O' && gameboard[3] === 'O' && gameboard[6] === 'O'
+            || gameboard[1] === 'O' && gameboard[4] === 'O' && gameboard[7] === 'O'
+            || gameboard[2] === 'O' && gameboard[5] === 'O' && gameboard[8] === 'O'
+            || gameboard[0] === 'O' && gameboard[1] === 'O' && gameboard[2] === 'O'
+            || gameboard[3] === 'O' && gameboard[4] === 'O' && gameboard[5] === 'O'
+            || gameboard[6] === 'O' && gameboard[7] === 'O' && gameboard[8] === 'O'
+            || gameboard[0] === 'O' && gameboard[4] === 'O' && gameboard[8] === 'O'
+            || gameboard[6] === 'O' && gameboard[4] === 'O' && gameboard[2] === 'O') {
+            console.log('O has won')
+            return 'O'
+        } else {
+            return false
+        }
+    }
+
+    return { gameboard, getCurrentPlayer, playerX, playerO, createPlayer, makeTurn, switchPlayer, getGameStatus }
+}
+
+function playGame() {
+    // Создать игру
+
+    game = gameActions();
+
+    document.querySelectorAll('.field').forEach((cell, index) => {
+        cell.addEventListener('click', () => {
+            cell.innerHTML = `${game.getCurrentPlayer().marker}`;
+            game.makeTurn(index);
+
+
+            const gameStatus = game.getGameStatus();
+            if (gameStatus === 'Draw') {
+                console.log('Draw');
+                document.querySelectorAll('.field').forEach((cell, index) => {
+                    cell.innerHTML = `draw`
+                })
+                return;
+            } else if (gameStatus === 'X') {
+                console.log('X has won');
+                document.querySelectorAll('.field').forEach((cell, index) => {
+                    cell.innerHTML = `X has won`
+                })
+                return;
+            } else if (gameStatus === 'O') {
+                console.log('O has won');
+                document.querySelectorAll('.field').forEach((cell, index) => {
+                    cell.innerHTML = `O has won`
+                })
+                return;
+            }
+        });
+    });
+
 }
