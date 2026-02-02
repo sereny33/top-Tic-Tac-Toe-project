37c41e98d2b95681a9c5a0de23cdc4a088ae16d0
Sergei Nosov <serejke33@gmail.com>
2026-02-02 18:47:01 +0300

    Fix game logic, add DOM manipulations

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
 function gameActions() {
-    // Создать массив-поле для игры
-    const gameboard = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-    // Создать игроков
+    // Create gameboard
+    let gameboard = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+
+    const resetGameboard = () => {
+        gameboard = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+        document.querySelectorAll('.field').forEach((cell) => {
+            cell.innerHTML = `-`;
+            cell.classList.remove('yellow');
+            cell.classList.remove('circle');
+            cell.classList.remove('cross');
+            document.getElementById('currentPlayer').innerHTML = ``;
+        });
+
+    }
+    // Create Players
     const createPlayer = (marker) => {
-        const name = prompt(`Игрок ${marker} Введите ваше имя`, `${marker}`)
-        return { name, marker }
+        const name = prompt(`Игрок ${marker} Введите ваше имя`, `${marker}`);
+        return { name, marker };
     }
 
-    // Создать игроков
     playerX = createPlayer('X');
     playerO = createPlayer('O');
 
-    // Текущий игрок в начале всегда Х
+    // Current player is always X in the beginning
     let currentPlayer = playerX;
 
+
+    // Allows to pass currentPlayer somwhere else
     const getCurrentPlayer = () => currentPlayer
-    // Смена текущего игрока
+    
+    // Change current player
     const switchPlayer = () => {
         (currentPlayer === playerX)
             ? currentPlayer = playerO
@@ -23,83 +38,108 @@ function gameActions() {
         return currentPlayer
     };
 
-    // Ход игрока
+    // Make turn 
     const makeTurn = (index) => {
         if (gameboard[index] !== 0) return;
 
         gameboard[index] = currentPlayer.marker
-        console.log('done')
 
         if (!getGameStatus()) {
             switchPlayer();
         }
     }
 
-    // Проверка состояния игры
+    // Check if anyone has won
     const getGameStatus = () => {
-        if (!gameboard.includes(0)) {
 
-            return 'Draw'
-        } else if (gameboard[0] === 'X' && gameboard[3] === 'X' && gameboard[6] === 'X'
-            || gameboard[1] === 'X' && gameboard[4] === 'X' && gameboard[7] === 'X'
-            || gameboard[2] === 'X' && gameboard[5] === 'X' && gameboard[8] === 'X'
-            || gameboard[0] === 'X' && gameboard[1] === 'X' && gameboard[2] === 'X'
-            || gameboard[3] === 'X' && gameboard[4] === 'X' && gameboard[5] === 'X'
-            || gameboard[6] === 'X' && gameboard[7] === 'X' && gameboard[8] === 'X'
-            || gameboard[0] === 'X' && gameboard[4] === 'X' && gameboard[8] === 'X'
-            || gameboard[6] === 'X' && gameboard[4] === 'X' && gameboard[2] === 'X') {
-            console.log('X has won')
-            return 'X'
-        } else if (gameboard[0] === 'O' && gameboard[3] === 'O' && gameboard[6] === 'O'
-            || gameboard[1] === 'O' && gameboard[4] === 'O' && gameboard[7] === 'O'
-            || gameboard[2] === 'O' && gameboard[5] === 'O' && gameboard[8] === 'O'
-            || gameboard[0] === 'O' && gameboard[1] === 'O' && gameboard[2] === 'O'
-            || gameboard[3] === 'O' && gameboard[4] === 'O' && gameboard[5] === 'O'
-            || gameboard[6] === 'O' && gameboard[7] === 'O' && gameboard[8] === 'O'
-            || gameboard[0] === 'O' && gameboard[4] === 'O' && gameboard[8] === 'O'
-            || gameboard[6] === 'O' && gameboard[4] === 'O' && gameboard[2] === 'O') {
-            console.log('O has won')
-            return 'O'
+        const winningConditions = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [6, 4, 2]
+        ];
+
+        const winningCombo = winningConditions.find(combo => {
+            const [firstElement, secondElement, thirdElement] = combo;
+            return (gameboard[firstElement] !== 0
+                && gameboard[firstElement] === gameboard[secondElement]
+                && gameboard[firstElement] === gameboard[thirdElement])
+        });
+
+        if (winningCombo) {
+            return {
+                winner: gameboard[winningCombo[0]],
+                winnerLine: winningCombo
+            }
+        }
+
+        if (!gameboard.includes(0)) {
+            return {
+                winner: 'Draw',
+            }
         } else {
-            return false
+            return false;
         }
     }
 
-    return { gameboard, getCurrentPlayer, playerX, playerO, createPlayer, makeTurn, switchPlayer, getGameStatus }
+    return { gameboard, getCurrentPlayer, playerX, playerO, createPlayer, makeTurn, switchPlayer, getGameStatus, resetGameboard }
 }
 
 function playGame() {
-    // Создать игру
 
+    // Starts new game
     game = gameActions();
 
+    // Changes cell signs
     document.querySelectorAll('.field').forEach((cell, index) => {
         cell.addEventListener('click', () => {
-            cell.innerHTML = `${game.getCurrentPlayer().marker}`;
+            if (game.gameboard[index] !== 0) return;
+
+            // cell.innerHTML = `${game.getCurrentPlayer().marker}`;
+            if (game.getCurrentPlayer().marker === "O") {
+                cell.classList.add('circle');
+            } else {
+                cell.classList.add('cross');
+            }
             game.makeTurn(index);
 
+            document.getElementById('currentPlayer').innerHTML = `Player ${game.getCurrentPlayer().marker} turn`;
 
             const gameStatus = game.getGameStatus();
-            if (gameStatus === 'Draw') {
-                console.log('Draw');
-                document.querySelectorAll('.field').forEach((cell, index) => {
+            if (gameStatus.winner === 'Draw') {
+                document.querySelectorAll('.field').forEach((cell) => {
                     cell.innerHTML = `draw`
-                })
+                });
                 return;
-            } else if (gameStatus === 'X') {
-                console.log('X has won');
-                document.querySelectorAll('.field').forEach((cell, index) => {
-                    cell.innerHTML = `X has won`
-                })
+            } else if (gameStatus.winner === "X") {
+                document.querySelectorAll('.field').forEach((cell) => {
+                    cell.innerHTML = `${game.playerX.name} has won`;
+                    gameStatus.winnerLine.forEach(index => {
+                        const yellowField = document.getElementById(index);
+                        yellowField.classList.add('yellow')
+                    })
+                });
                 return;
-            } else if (gameStatus === 'O') {
-                console.log('O has won');
-                document.querySelectorAll('.field').forEach((cell, index) => {
-                    cell.innerHTML = `O has won`
-                })
+            } else if (gameStatus.winner === "O") {
+                document.querySelectorAll('.field').forEach((cell) => {
+                    cell.innerHTML = `${game.playerO.name} has won`;
+                    gameStatus.winnerLine.forEach(index => {
+                        const yellowField = document.getElementById(index);
+                        yellowField.classList.add('yellow')
+                    });
+                });
                 return;
             }
         });
     });
-
+    
+    // Resets gameboard for a new game
+    document.getElementById('reset-btn').addEventListener('click', () => {
+        game.resetGameboard();
+        return;
+    })
 }
